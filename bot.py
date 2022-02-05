a65668796492aaff0f5e1b44f5e5018b6433af12
TechPertz <reetnandy@gmail.com>
2022-02-05 10:48:33 +0530

    custom buttons

--- bot.py
+++ bot.py
@@ -1,17 +1,31 @@
-import requests
-import time
+from telegram import * 
 from telegram.ext import * 
 from information import *
+# from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
+
+def start(update: Update, context: CallbackContext):
+    kb = [[KeyboardButton('Stores')],
+          [KeyboardButton('Forms')],
+          [KeyboardButton('Giveaways')],
+          [KeyboardButton('Tickets')],
+          [KeyboardButton('Vouchers')]
+          ]
+    kb_markup = ReplyKeyboardMarkup(kb)
+    context.bot.send_message(chat_id=update.message.chat_id,
+                     text="your message",
+                     reply_markup=kb_markup)
+
 
 def handle_message(update , context):
-    text = str(update.message.text).lower()
-
     update.message.reply_text(f"Hi, {update['message']['chat']['first_name']}")
+
+    # update.message.reply(reply_markup=keyboard1)
 
 if __name__ == '__main__':
     updater = Updater(BOT_API , use_context=True)
     dp = updater.dispatcher
 
+    dp.add_handler(CommandHandler("start", start))
     dp.add_handler(MessageHandler(Filters.text , handle_message))
 
     updater.start_polling(1.0)
