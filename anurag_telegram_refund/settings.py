8a6e1600773452a9157ec87628f4493369346471
TechPertz <reetnandy@gmail.com>
2022-02-13 09:22:24 +0530

    info

--- anurag_telegram_refund/settings.py
+++ anurag_telegram_refund/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from information import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)k@#+%3803wn0#mp3w%q3%ahw-k)9na^05ak(!rrry5u%$li5x'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- information.py
+++ information.py
@@ -1,1 +1,2 @@
 BOT_API = "5106536390:AAEZEYoM1Z25FVVeqZaayoU50bTn1HNHFCs"
+SECRET_KEY = 'django-insecure-)k@#+%3803wn0#mp3w%q3%ahw-k)9na^05ak(!rrry5u%$li5x'
